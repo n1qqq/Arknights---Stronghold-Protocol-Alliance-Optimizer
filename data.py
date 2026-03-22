0bdd1bdfc68726521676eca614994ed9775da0a3
n1qqq <n1qqq@foxmail.com>
2026-03-21 17:48:33 -0700

    Backbone functionality with specific data from a single game. Will update data.py later.

---
+++ data.py
@@ -0,0 +1,39 @@
+# data.py
+from scaffold import Chara
+
+chara_pool = {
+    'Muelsyse': Chara('Yan', 'Sargon', 'Victoria', 'Kjerag', 'Laterano', 'Aegir', 'Siracusa', 'Kazimierz', 'Harmony'),
+
+    'Leizi': Chara('Yan'), 'Swire': Chara('Yan'), 'Swire the Elegant Wit': Chara('Yan'), 'Record Keeper': Chara('Yan'), 'Dusk': Chara('Arcane', 'Yan'),
+    'Yu': Chara('Bastion', 'Yan'),
+
+    'Titi': Chara('Sargon', 'Precision'), 'Passenger': Chara('Sargon', 'Swift', 'Solitary'), 'Minimalist': Chara('Sargon', 'Agility'), 'Bubble': Chara('Sargon', 'Bastion'), 'Beeswax': Chara('Sargon', 'Support'),
+    'Pepe': Chara('Sargon', 'Tenacity'), 'Estelle': Chara('Sargon'), 'Papyrus': Chara('Sargon'), 'Philae': Chara('Sargon'), 'Gavial the Invincible': Chara('Sargon'),
+
+    'Vendela': Chara('Victoria', 'Support'), 'Rockrock': Chara('Victoria', 'Arcane'), 'Bagpipe': Chara('Victoria', 'Tenacity'), 'Vina Victoria': Chara('Victoria'),
+    'Harold': Chara('Victoria', 'Kjerag'),
+    'Matterhorn': Chara('Kjerag', 'Bastion'), 'Snow Hunter': Chara('Kjerag', 'Precision'), 'SilverAsh the Reignfrost': Chara('Kjerag', 'Swift'), 'Gnosis': Chara('Kjerag', 'Agility'),
+    'Pramanix the Prerita': Chara('Kjerag', 'Arcane'), 'Kjera': Chara('Kjerag', 'Support'), 'Pramanix': Chara('Kjerag'), 'Cliffheart': Chara('Kjerag', 'Tenacity'),
+    'Degenbrecher': Chara('Kjerag', 'Kazimierz', 'Swift'),
+    'Wild Mane': Chara('Kazimierz', 'Swift'), 'Ashlock': Chara('Kazimierz', 'Bastion'), 'Gravel': Chara('Kazimierz', 'Tenacity'),
+
+    'Insider': Chara('Laterano', 'Swift'), 'Executor': Chara('Laterano', 'Precision'), 'Lemuen': Chara('Laterano', 'Precision'), 'Mostima': Chara('Laterano', 'Arcane'), 'Exusiai': Chara('Laterano'),
+    'Sankta Miksaparato': Chara('Laterano', 'Bastion'), 'Executor the Ex Foedere': Chara('Laterano'), 'Enforcer': Chara('Laterano'), 'Virtuosa': Chara('Laterano'), 'Exusiai the New Covenant': Chara('Laterano'),
+
+    'Underflow': Chara('Aegir'), 'Lucilla': Chara('Aegir', 'Arcane'), 'Skadi': Chara('Aegir', 'Bastion', 'Assault'), 'Specter the Unchained': Chara('Aegir', 'Tenacity'), 'Mizuki': Chara('Aegir', 'Solitary'),
+    'Specter': Chara('Aegir'), 'Gladiia': Chara('Aegir'), 'Ulpianus': Chara('Aegir'), 'Skadi the Corrupting Heart': Chara('Aegir'),
+
+    'Provence': Chara('Siracusa'), 'Texas': Chara('Siracusa', 'Solitary'), 'Vulpisfoglia': Chara('Siracusa', 'Swift'), 'Aroma': Chara('Siracusa', 'Arcane'), 'Shamare': Chara('Siracusa', 'Support'),
+    'Angelina': Chara('Siracusa'), 'Texas the Omertosa': Chara('Siracusa', 'Assault'), 'Suzuran': Chara('Siracusa', 'Cover'), 'Lappland': Chara('Siracusa'), 'Lappland the Decadenza': Chara('Siracusa'),
+
+
+    'Gum': Chara('Bastion'), 'Vetochki': Chara('Bastion', 'Solitary'), 'Cuora': Chara('Bastion', 'Tenacity'), 'Saria': Chara('Bastion', 'Solitary'),
+    'Vigna': Chara('Tenacity'), 'Entelechia': Chara('Tenacity', 'Agility'),
+    'Ines': Chara('Assault'), 'Surtr': Chara('Assault', 'Arcane'),
+    'Tippi': Chara('Swift', 'Agility'), 'Pinecone': Chara('Swift', 'Cover'), 'Leonhardt': Chara('Swift', 'Precision'), 'Thorns the Lodestar': Chara('Swift'), 'Nymph': Chara('Swift'),
+    'Rosmontis': Chara('Precision', 'Cover'),
+    'Indigo': Chara('Arcane'), 'Akkord': Chara('Arcane', 'Support'), 'Astgenne the Lightchaser': Chara('Arcane', 'Agility'),
+    'Ptilopsis': Chara('Agility', 'Support'),
+    'Podenco': Chara('Support'), 'Perfumer': Chara('Support'), 'Lumen': Chara('Support'), 'Eyjafjalla the Hvit Aska': Chara('Support'),
+    'Rose Salt': Chara('Cover'), 'Civilight Eterna': Chara('Cover')
+}
